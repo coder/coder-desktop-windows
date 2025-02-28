01222506ae2067d60de4eb911cc932d3ce031230
Ethan <39577870+ethanndickson@users.noreply.github.com>
2025-02-28 22:12:21 +1100

    fix

--- App/Converters/AgentStatusToColorConverter.cs
+++ App/Converters/AgentStatusToColorConverter.cs
@@ -9,9 +9,9 @@ namespace Coder.Desktop.App.Converters;
 public class AgentStatusToColorConverter : IValueConverter
 {
     private static readonly SolidColorBrush Green = new(Color.FromArgb(255, 52, 199, 89));
+    private static readonly SolidColorBrush Yellow = new(Color.FromArgb(255, 204, 1, 0));
     private static readonly SolidColorBrush Red = new(Color.FromArgb(255, 255, 59, 48));
     private static readonly SolidColorBrush Gray = new(Color.FromArgb(255, 142, 142, 147));
-    private static readonly SolidColorBrush Yellow = new(Color.FromArgb(255, 204, 1, 0));
 
     public object Convert(object value, Type targetType, object parameter, string language)
     {
