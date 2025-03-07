7fc6398bbb6c7568341b2eae38a99da6c6552440
Dean Sheather <dean@deansheather.com>
2025-03-07 18:14:25 +1100

    chore: enable tunnel binary verification (#36)
    - Enables assembly version verification
    - Enables authenticode verification
    - Adds local machine registry config options to enable/disable either of
    these
    
    Closes #41 
    Closes #45

--- Installer/Program.cs
+++ Installer/Program.cs
@@ -250,13 +250,17 @@ public class Program
         programFiles64Folder.AddDir(installDir);
         project.AddDir(programFiles64Folder);
 
-        // Add registry values that are consumed by the manager.
+        // Add registry values that are consumed by the manager. Note that these
+        // should not be changed. See Vpn.Service/Program.cs and
+        // Vpn.Service/ManagerConfig.cs for more details.
         project.AddRegValues(
             new RegValue(RegistryHive, RegistryKey, "Manager:ServiceRpcPipeName", "Coder.Desktop.Vpn"),
             new RegValue(RegistryHive, RegistryKey, "Manager:TunnelBinaryPath",
                 $"[INSTALLFOLDER]{opts.VpnDir}\\coder-vpn.exe"),
             new RegValue(RegistryHive, RegistryKey, "Manager:LogFileLocation",
-                @"[INSTALLFOLDER]coder-desktop-service.log"));
+                @"[INSTALLFOLDER]coder-desktop-service.log"),
+            new RegValue(RegistryHive, RegistryKey, "Manager:TunnelBinarySignatureSigner", "Coder Technologies Inc."),
+            new RegValue(RegistryHive, RegistryKey, "Manager:TunnelBinaryAllowVersionMismatch", "false"));
 
         // Note: most of this control panel info will not be visible as this
         // package is usually hidden in favor of the bootstrapper showing
