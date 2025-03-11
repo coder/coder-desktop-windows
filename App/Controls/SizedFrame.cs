dd0490033cc217136ba85fbee0126e4b7d87340b
Dean Sheather <dean@deansheather.com>
2025-03-11 19:01:15 +1100

    chore: add SizedFrame for window sizing

---
+++ App/Controls/SizedFrame.cs
@@ -0,0 +1,65 @@
+using System;
+using Windows.Foundation;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Coder.Desktop.App.Controls;
+
+public class SizedFrameEventArgs : EventArgs
+{
+    public Size NewSize { get; init; }
+}
+
+/// <summary>
+///     SizedFrame extends Frame by adding a SizeChanged event. Sadly this is necessary because
+///     Window.Content.SizeChanged doesn't trigger when the Page's content changes.
+/// </summary>
+public class SizedFrame : Frame
+{
+    public delegate void SizeChangeDelegate(object sender, SizedFrameEventArgs e);
+
+    public new event SizeChangeDelegate? SizeChanged;
+
+    private Size _lastSize;
+
+    public void SetPage(Page page)
+    {
+        if (ReferenceEquals(page, Content)) return;
+
+        // Set the new event listener.
+        if (page.Content is not FrameworkElement newElement)
+            throw new Exception("Failed to get Page.Content as FrameworkElement on SizedFrame navigation");
+        newElement.SizeChanged += Content_SizeChanged;
+
+        // Unset the previous event listener.
+        if (Content is Page { Content: FrameworkElement oldElement })
+            oldElement.SizeChanged -= Content_SizeChanged;
+
+        // We don't use RootFrame.Navigate here because it doesn't let you
+        // instantiate the page yourself. We also don't need forwards/backwards
+        // capabilities.
+        Content = page;
+
+        // Fire an event.
+        Content_SizeChanged(newElement, null);
+    }
+
+    public Size GetContentSize()
+    {
+        if (Content is not Page { Content: FrameworkElement frameworkElement })
+            throw new Exception("Failed to get Content as FrameworkElement for SizedFrame");
+
+        frameworkElement.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+        return new Size(frameworkElement.ActualWidth, frameworkElement.ActualHeight);
+    }
+
+    private void Content_SizeChanged(object sender, SizeChangedEventArgs? _)
+    {
+        var size = GetContentSize();
+        if (size == _lastSize) return;
+        _lastSize = size;
+
+        var args = new SizedFrameEventArgs { NewSize = size };
+        SizeChanged?.Invoke(this, args);
+    }
+}
--- App/Views/SignInWindow.xaml.cs
+++ App/Views/SignInWindow.xaml.cs
@@ -1,18 +1,20 @@
+using System;
 using Windows.Graphics;
+using Coder.Desktop.App.Controls;
 using Coder.Desktop.App.ViewModels;
 using Coder.Desktop.App.Views.Pages;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
 
 namespace Coder.Desktop.App.Views;
 
 /// <summary>
-///     The dialog window to allow the user to sign into their Coder server.
+///     The dialog window to allow the user to sign in to their Coder server.
 /// </summary>
 public sealed partial class SignInWindow : Window
 {
-    private const double WIDTH = 600.0;
-    private const double HEIGHT = 300.0;
+    private const double WIDTH = 500.0;
 
     private readonly SignInUrlPage _signInUrlPage;
     private readonly SignInTokenPage _signInTokenPage;
@@ -20,8 +22,17 @@ public sealed partial class SignInWindow : Window
     public SignInWindow(SignInViewModel viewModel)
     {
         InitializeComponent();
+        SystemBackdrop = new DesktopAcrylicBackdrop();
+        RootFrame.SizeChanged += RootFrame_SizeChanged;
+
         _signInUrlPage = new SignInUrlPage(this, viewModel);
         _signInTokenPage = new SignInTokenPage(this, viewModel);
+
+        // Prevent the window from being resized.
+        if (AppWindow.Presenter is not OverlappedPresenter presenter)
+            throw new Exception("Failed to get OverlappedPresenter for window");
+        presenter.IsMaximizable = false;
+        presenter.IsResizable = false;
 
         NavigateToUrlPage();
         ResizeWindow();
@@ -30,20 +41,32 @@ public sealed partial class SignInWindow : Window
 
     public void NavigateToTokenPage()
     {
-        RootFrame.Content = _signInTokenPage;
+        RootFrame.SetPage(_signInTokenPage);
     }
 
     public void NavigateToUrlPage()
     {
-        RootFrame.Content = _signInUrlPage;
+        RootFrame.SetPage(_signInUrlPage);
+    }
+
+    private void RootFrame_SizeChanged(object sender, SizedFrameEventArgs e)
+    {
+        ResizeWindow(e.NewSize.Height);
     }
 
     private void ResizeWindow()
     {
+        ResizeWindow(RootFrame.GetContentSize().Height);
+    }
+
+    private void ResizeWindow(double height)
+    {
+        if (height <= 0) height = 100; // will be resolved next frame typically
+
         var scale = DisplayScale.WindowScale(this);
-        var height = (int)(HEIGHT * scale);
-        var width = (int)(WIDTH * scale);
-        AppWindow.Resize(new SizeInt32(width, height));
+        var newWidth = (int)(WIDTH * scale);
+        var newHeight = (int)(height * scale);
+        AppWindow.ResizeClient(new SizeInt32(newWidth, newHeight));
     }
 
     private void MoveWindowToCenterOfDisplay()
